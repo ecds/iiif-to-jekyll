c9548026eea70a2db1cba265033e1b1fc6fd7638
Ben W. Brumfield <benwbrum@gmail.com>
2019-06-20 09:33:34 -0500

    Improved documentation

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # IiifToJekyll
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/iiif_to_jekyll`. To experiment with that code, run `bin/console` for an interactive prompt.
+IiifToJekyll is designed to produce a static digital edition site from the 
+Readux digital editing platform.  The library reads from a IIIF manifest and
+annotation server, writing to a Jekyll template/theme directory.
 
-TODO: Delete this and the text above, and describe your gem
+## Dependencies
+
+IiifToJekyll currently depends on a IIIF Bundle exported from Readux as its input 
+manifest (while pulling annotations from the live Readux site), but should
+be adaptible to work with any IIIF manifest and set of annotations.
+
+IiifToJekyll adds data files to a Jekyll theme directory extracted from
+https://github.com/ecds/digitaledition-jekylltheme
 
 ## Installation
 
@@ -22,7 +31,13 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+For command-line usage, run 
+	$ ./bin/iiif_to_jekyll export_dir digitaledition-jekylltheme_dir
+
+After this has been run, you can see your exported edition:
+	$ cd digitaledition-jekylltheme_dir
+	$ bundle update
+	$ jekyll serve
 
 ## Development
 
@@ -30,14 +45,8 @@ After checking out the repo, run `bin/setup` to install dependencies. Then, run 
 
 To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
-## Contributing
-
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/iiif_to_jekyll. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
 
 ## License
 
 The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
 
-## Code of Conduct
-
-Everyone interacting in the IiifToJekyll project’s codebases, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/[USERNAME]/iiif_to_jekyll/blob/master/CODE_OF_CONDUCT.md).
--- lib/iiif_to_jekyll/annotation.rb
+++ lib/iiif_to_jekyll/annotation.rb
@@ -1,13 +1,19 @@
 # Class representing the data we need from WebAnnotations fetched from Readux
+# Attributes like `x_px` are data parsed from individual WebAnnotations
+# Helper methods like `left_pct` translate raw data into derivative formats 
+# needed for Jekyll
+# Static methods like `from_oa` or `ocr_annotations` parse WebAnnotation JSON
+# hashes and AnnotationLists into usable collections of Annotation objects.
 class Annotation
   attr_accessor :x_px, :y_px, :w_px, :h_px, :motivation, :text, :user, :anno_id, :canvas
 
+  # Constants used for distinguishing OCR annotations from scholarly commentary
   module Motivation
     COMMENTING = "sc:commenting"
     PAINTING = "sc:painting"
   end
 
-  # Sample source
+  # Sample source annotation; for reference only
   EXAMPLE_OCR_SOURCE =
    {"@context"=>"http://iiif.io/api/presentation/2/context.json",
     "@id"=>"f87fef89-8176-4416-a7bb-ec0403016189",
@@ -36,13 +42,20 @@ class Annotation
     '</div>'
 
 
+  # Factory method creating Annotation objects from hashes created by
+  # parsing individual JSON WebAnnotations.
   def self.from_oa(json_hash, canvas)
     anno = Annotation.new
+
+    # reference to canvas object
     anno.canvas = canvas
+
+    # simple attributes
     anno.text = json_hash['resource']['chars']
     anno.anno_id = json_hash['@id']
     anno.motivation = json_hash['motivation']
 
+    # complex/parsed attributes
     selector = json_hash['on']['selector']['value']
     md = selector.match /(\d+),(\d+),(\d+),(\d+)/
     anno.x_px = md[1].to_i
@@ -53,19 +66,22 @@ class Annotation
     anno
   end
 
-  # TODO round these off to nearest hundredth
+  # helper method for HTML positioning
   def left_pct
     (100 * x_px.to_f / canvas.width).floor(2)
   end
 
+  # helper method for HTML positioning
   def top_pct
     (100 * y_px.to_f / canvas.height).floor(2)
   end
 
+  # helper method for HTML positioning
   def width_pct
     (100 * w_px.to_f / canvas.width).floor(2)
   end
 
+  # helper method for HTML positioning
   def height_pct
     (100 * h_px.to_f / canvas.height).floor(2)
   end
@@ -75,22 +91,28 @@ class Annotation
     "20px"  # this is specified in pixels, but surely that's wrong?
   end
 
+
+  # factory method for creating an arry of Annotation objectss from a raw hash
+  # parsed from an AnnotationList 
   def self.all_annotations(annotation_list_json, canvas)
     annotations = []
     annotation_list_json['resources'].each do |anno_json|
       annotations << Annotation.from_oa(anno_json, canvas)
     end
+
     annotations
   end
 
+  # factory method parsing only OCR annotations
   def self.ocr_annotations(annotation_list_json, canvas)
     annotations = all_annotations(annotation_list_json, canvas)
-    annotations.keep_if { |anno| anno.motivation == Motivation::PAINTING }
+    annotations.reject { |anno| anno.motivation != Motivation::PAINTING }
   end
 
+  # factory method parsing only commentary annotations
   def self.comment_annotations(annotation_list_json, canvas)
     annotations = all_annotations(annotation_list_json, canvas)
-    annotations.keep_if { |anno| anno.motivation == Motivation::COMMENTING }
+    annotations.reject { |anno| anno.motivation != Motivation::COMMENTING }
   end
 
 
